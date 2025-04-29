9bddc24682fab64b2af9b349b2815d62fd01842c
literate-t <gaekoon@gmail.com>
2025-04-29 22:43:40 +0900

    feat: 드래그앤드랍을 위해 이벤트 핸들러를 추가한다

--- Assets/Scripts/UI/UI_EventHandler.cs
+++ Assets/Scripts/UI/UI_EventHandler.cs
@@ -6,9 +6,22 @@ public class UI_EventHandler : MonoBehaviour, IDragHandler, IPointerClickHandler
 {
     public Action<PointerEventData> OnClickHandler = null;
     public Action<PointerEventData> OnDragHandler = null;
+    public Action<PointerEventData> OnBeginDragHandler = null;
+    public Action<PointerEventData> OnEndDragHandler = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        OnBeginDragHandler?.Invoke(eventData);
+    }
+
     public void OnDrag(PointerEventData eventData)
     {
         OnDragHandler?.Invoke(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        OnEndDragHandler?.Invoke(eventData);
     }
 
     public void OnPointerClick(PointerEventData eventData)
