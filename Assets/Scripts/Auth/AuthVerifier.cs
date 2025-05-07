f36019dcf18f0b4d3c8d3bd22656ab46038bab70
literate-t <gaekoon@gmail.com>
2025-05-07 21:48:38 +0900

    feat: AuthVerifier 클래스를 정의한다
    - 웹 로그인 시 토큰을 발급한 서버에 인증을 요청한다
    
    - 코루틴을 활용해 SendWebRequest() 함수를 호출한다
      - 비동기 함수이기 때문에 코루틴을 사용하지 않으면 대기하지 않는다

---
+++ Assets/Scripts/Auth/AuthVerifier.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AuthVerifier : MonoBehaviour
+{
+    const string TokenVerifyUrl = "localhost:8080/api/auth/verify-access-token";
+    const string Authorization = "Authorization";
+
+    IEnumerator Start()
+    {
+        string accessToken = Launcher.AuthToken;
+        if (accessToken == null)
+        {
+            Debug.LogError("AuthVerifier: No Access token found");
+            Launcher.ForceQuit();
+            yield break;
+        }
+
+        UnityWebRequest req = UnityWebRequest.Get(TokenVerifyUrl);
+        req.SetRequestHeader(Authorization, $"Bearer {accessToken}");
+        yield return req.SendWebRequest();
+
+        if (req.result != UnityWebRequest.Result.Success || req.responseCode != 200)
+        {
+            Debug.LogError("AuthVerifier: Token verifying failed");
+            Launcher.ForceQuit();
+            yield break;
+        }
+
+        Managers.Scene.LoadScene(Define.Scene.Game);
+    }
+}
