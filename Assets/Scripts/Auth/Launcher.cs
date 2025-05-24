a6ac52d84db60a9bf22bc67679b5ce9f2eded734
literate-t <gaekoon@gmail.com>
2025-05-24 21:42:44 +0900

    feat: 사용자의 Id를 웹에서 넘겨받아 별도로 파싱해 프로퍼티에 추가한다

--- Assets/Scripts/Auth/Launcher.cs
+++ Assets/Scripts/Auth/Launcher.cs
@@ -1,12 +1,11 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class Launcher : MonoBehaviour
 {
     public static string AuthToken { get; private set; }
+    public static string Id { get; private set; }    
 
     private void Awake()
     {
@@ -21,23 +20,39 @@ public class Launcher : MonoBehaviour
         }
 
         Uri uri = new(uriArg);
-        Dictionary<string, string> query = uri.Query
+
+        var queries = uri.Query
             .TrimStart('?')
             .Split('&')
-            .Select(param => param.Split('='))
-            .ToDictionary(item => item[0], item => Uri.UnescapeDataString(item[1]));
+            .Select(param => param.Split('='));
+
+        var dic_query = queries.ToDictionary(
+            item => item[0],
+            item => item[1], StringComparer.OrdinalIgnoreCase);
 
         string result;
-        if (!query.TryGetValue("token", out result))
+        if (!dic_query.TryGetValue("token", out result))
         {
-            Debug.LogError("Launcer: Parsing token failed");
+            Debug.LogError("Launcher: Parsing token failed");
             ForceQuit();
             return;
         }
 
-        AuthToken = result;
+        AuthToken = Uri.UnescapeDataString(result);
 
-        DontDestroyOnLoad(this);
+        if (!dic_query.TryGetValue("email", out result))
+        {
+            Debug.LogError("Launcher: Parsing email failed");
+            ForceQuit();
+            return;
+        }
+
+        Id = result.Substring(0, result.IndexOf('@'));
+
+        DontDestroyOnLoad(gameObject);
+
+        AuthVerifier authVerifier = new();
+        StartCoroutine(authVerifier.Verify());
     }
 
     public static void ForceQuit()
